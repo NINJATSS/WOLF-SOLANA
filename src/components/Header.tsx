bcf2b6f620deeb08236536c299d91b34d36a737a
Your Name <you@example.com>
2023-09-30 15:00:15 +0200

    fixed header icons and stuff

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './Header.module.css'
+import { FaDiscord, FaTwitter, FaGlobeAmericas} from 'react-icons/fa';
 
 export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
@@ -8,21 +9,44 @@ export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
       <div>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <NavLink className={styles.logo} to="/">
-            <img alt="WOLF" src="/logo-2.png" />
+            <img alt="WOLF" src="/logo.png" />
           </NavLink>
         </div>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-          <div className={styles.externalLinks}>
-            <a href="https://discord.gg/mXmPtZykJK" target="_blank" rel="noreferrer">
-              Discord
+          <div className={styles.iconContainer}>
+            <a href="https://discord.gg/mXmPtZykJK" target="_blank" rel="noreferrer" className={styles.icon}>
+              <FaDiscord />
             </a>
-            <a href="https://twitter.com/WolfSolanaNFT" target="_blank" rel="noreferrer">
-              Twitter
+            <a href="https://twitter.com/WolfSolanaNFT" target="_blank" rel="noreferrer" className={styles.icon}>
+              <FaTwitter />
             </a>
           </div>
+          <div style={{ display: 'flex', gap: '10px' }}>
           {children}
+        </div>
         </div>
       </div>
     </div>
   )
 }
+
+
+
+// export const Header: React.FC<React.PropsWithChildren<any>> = ({ children }) => {
+//   return (
+//     <div className={styles.wrapper}>
+//       <div>
+//         <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+//           <NavButton variant="ghost" className={styles.logo} to="/">
+//             <img src="/logo.png" height="20px" alt="Logo" />
+//             <span className={styles.title}>PEPESOLANA CASINO</span>
+//           </NavButton>
+          
+//         </div>
+        // <div style={{ display: 'flex', gap: '10px' }}>
+        //   {children}
+        // </div>
+//       </div>
+//     </div>
+//   );
+// };
